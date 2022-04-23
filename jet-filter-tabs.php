d7307cb098fdf9369dafad53d7c09b490cc48382
dislokacia <44621240+dislokacia@users.noreply.github.com>
2022-04-24 00:23:32 +0300

    register scripts

--- jet-filter-tabs.php
+++ jet-filter-tabs.php
@@ -17,9 +17,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 
 function jet_filter_tabs_scripts(){
-	wp_register_script( 'jet_filter_tabs', plugins_url( '/assets/js/script.js', __FILE__ ), array( 'jquery' ), '1.0', true );
-	wp_enqueue_script( 'jet_filter_tabs' );
- 
+	wp_enqueue_script( 'jet_filter_tabs', plugins_url( '/assets/js/script.js', __FILE__ ), array( 'jquery' ), '1.0', true ); 
 	wp_enqueue_style( 'jet_filter_tabs_styles', plugins_url( '/assets/css/style.css', __FILE__ ), '', '1.0' );
 }
 add_action( 'jet-engine/listings/frontend-scripts', 'jet_filter_tabs_scripts' );
